c3224b3e46a5cdf28a92f749900f040397020a38
DarkSpear <admin@lawal.org>
2026-09-12 18:09:53 +0100

    config: connect TEACHR subscription checkout

--- payment-config.js
+++ payment-config.js
@@ -1,3 +1,3 @@
 window.TEACHR_PAYMENT = Object.freeze({
-  appsScriptUrl: ''
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxDo2k7C88inC3LdBi7Sr9m_ZQagWUHcEt-Bpgql4sxB2UgR0p7m0gYT5q0153MAddrXQ/exec'
 });
